d8233b40ce914790a599bdde7f0c8f4c5ad8aa70
stephanos <github@stephanbehnke.de>
2013-06-21 16:37:54 +0200

    initial import

---
+++ src/com/loops101/icons/GoIconProvider.java
@@ -0,0 +1,32 @@
+package com.loops101.icons;
+
+import com.intellij.ide.IconProvider;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class GoIconProvider extends IconProvider {
+
+    private static final String SOURCE_URL = "/com/loops101/icons/go.png";
+    private static final String TEST_URL = "/com/loops101/icons/go-test.png";
+
+    public Icon getIcon(@NotNull PsiElement psiElement, int flags) {
+
+        PsiFile containingFile = psiElement.getContainingFile();
+
+        if (containingFile != null) {
+            if (containingFile.getName().endsWith(".go")) {
+                if (containingFile.getName().endsWith("_test.go")) {
+                    return IconLoader.getIcon(TEST_URL);
+                } else {
+                    return IconLoader.getIcon(SOURCE_URL);
+                }
+            }
+        }
+
+        return null;
+    }
+}
